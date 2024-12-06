c2eac28dd3872664223690abbf97845f536be55f
Kevin Wang <kevinwang@smu.edu>
2024-12-05 19:34:54 -0600

    [Release Prep] Add metadata for BiGER
    Add the current descriptions of the package and its dependencies.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 VERSION = "0.0.1"
 
 setuptools.setup(
-    name = "PythonTemplate",
+    name = "BiGER",
     version = VERSION,
-    description = "A Template for your Python Package",
+    description = "A Python implementation of BiGER for Bayesian Aggregation in Genomics with Extended Ranking Schemes",
     long_description_content_type = "text/markdown",
     long_description = open("README.md").read(),
-    packages=["PythonTemplate"],
+    packages=["BiGER"],
     python_requires=">=3.9",
     install_requires=[],
     test_requires=["pytest",
